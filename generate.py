e446f588cda26ccae2d8bc2338ff40e95b236b16
Olga Sentemova <osentemova@aligntech.com>
2016-10-31 17:43:55 +0300

    Add multiprocessing

--- generate.py
+++ generate.py
@@ -6,14 +6,23 @@ import string
 import os
 import zipfile
 import logging
+import multiprocessing
+import csv
+
+from contextlib import closing
 
 logging.basicConfig(filename="log.txt", level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-NUMBER_XMLS_IN_FOLDER = 10
-NUMBER_OF_ZIPS = 1
+NUMBER_XMLS_IN_FOLDER = 100
+NUMBER_OF_ZIPS = 50
 
 ROOT_PATH = os.path.abspath(os.curdir)
+ZIP_DIR = "zips"
+
+LEVELS_CSV_NAME = "levels.csv"
+OBJECTS_CSV_NAME = "objects.csv"
+
 
 def random_string(len):
     return "".join(random.choice(string.uppercase + string.lowercase)
@@ -39,7 +48,8 @@ def create_xml():
     return ET.tostring(root, "utf-8")
 
 def create_zip(zipname):
-    path = os.path.join(ROOT_PATH, zipname)
+    path = os.path.join(ROOT_PATH, ZIP_DIR, zipname)
+
     with zipfile.ZipFile(path, "w") as zf:
         for i in range(NUMBER_XMLS_IN_FOLDER):
             filename = str(i) + ".xml"
@@ -69,10 +79,14 @@ def extract_xml(path_to_zip):
                 result["levels"].append(parsed_data["id"] +" "+ parsed_data["level"])
                 for obj in parsed_data["objects"]:
                     result["objects"].append(parsed_data["id"] +" "+ obj)
-
     return result
 
+def write_to_csv(csv_path, data):
 
+    with open(csv_path, "ab") as csvf:
+
+        writer = csv.writer(csvf, delimiter=' ', quoting=csv.QUOTE_NONE, escapechar="|")
+        writer.writerows(data)
 
 if __name__ == "__main__":
 
@@ -80,8 +94,22 @@ if __name__ == "__main__":
         zipname = str(i) + ".zip"
         create_zip(zipname)
 
-    for zipname in os.listdir(ROOT_PATH):
+    all_zips = []
+    extracted_data = []
+    for zipname in os.listdir(os.path.join(ROOT_PATH, ZIP_DIR)):
         if zipname.endswith(".zip"):
-            path_to_zip = os.path.join(ROOT_PATH, zipname)
+            all_zips.append(os.path.join(ROOT_PATH, ZIP_DIR, zipname))
 
-            print extract_xml(path_to_zip)
+    with closing(multiprocessing.Pool(processes=2)) as pool:
+        results = pool.map(extract_xml, all_zips, chunksize=1)
+        pool.terminate()
+
+    if os.path.isfile(LEVELS_CSV_NAME):
+        os.remove(LEVELS_CSV_NAME)
+
+    if os.path.isfile(OBJECTS_CSV_NAME):
+        os.remove(OBJECTS_CSV_NAME)
+
+    for result in results:
+        write_to_csv(LEVELS_CSV_NAME, result["levels"])
+        write_to_csv(OBJECTS_CSV_NAME, result["objects"])
